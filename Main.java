def5b5797d5d14b4e5a89c1206a894f91ae7e139
Saulux2005 <97383079+Saulux2005@users.noreply.github.com>
2022-04-20 19:25:01 +0300

    Added new files and directories

---
+++ Main.java
@@ -0,0 +1,73 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("Labas!");
+//        System.out.println("Koks tavo vardas?");
+        /*String name = "Saulius";
+//        String name = sc.nextLine();
+
+//        System.out.println("Labas, " + name + ", kokia tavo pavardė?");
+        String surname = "Krajeckas";
+//        String surname = sc.nextLine();
+        String fullname = name + " " + surname;
+
+        System.out.println("Labas, " + fullname);
+
+        /*int x, y, sum;
+        System.out.println("Įveskite du skaičius ir spauskite ENTER");
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        sum = x + y;
+        System.out.println("Skaičių suma yra" + sum);
+
+        String temp = sc.nextLine();
+        System.out.println("Temp yra " + temp);*/
+
+        /*String x, y, sum;
+        System.out.println("Įveskite du skaičius ir spauskite ENTER");
+        x = sc.nextLine();
+        y = sc.nextLine();
+
+
+
+        sum = "" + (Integer.parseInt(x) + Integer.parseInt(y));
+        System.out.println("Skaičių suma yra " + sum);
+
+//        String temp = sc.nextLine();
+//        System.out.println("Temp yra " + temp);*/
+
+        /*System.out.println("Labas!");
+        System.out.println("Koks tavo vardas?");
+//        String name = "Saulius";
+        String name = sc.nextLine();
+
+        System.out.println("Labas, " + name + ", kokia tavo pavardė?");
+//        String surname = "Krajeckas";
+        String surname = sc.nextLine();
+        String fullname = name + " " + surname;
+
+        System.out.println("Labas, " + fullname);
+
+        int x, y, z, sum;
+        System.out.println("Įveskite  pirmą skaičių");
+        x = sc.nextInt();
+        System.out.println("Įveskite  antrą skaičių");
+        y = sc.nextInt();
+        System.out.println("Įveskite  trečią skaičių");
+        z = sc.nextInt();
+        sum = x + y + z;
+        System.out.println("Skaičių suma yra " + sum);
+        System.out.println("Viso gero!");*/
+
+
+
+    }
+}
